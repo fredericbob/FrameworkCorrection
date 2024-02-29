c9c035b6697d348c58372aa1d08bd1ce1ebcf09c
frederic <lalafrederic05@gmail.com>
2024-02-29 07:50:06 +0300

    Sprint2

--- Framework/src/etu1283/framework/servlet/FrontServlet.java
+++ Framework/src/etu1283/framework/servlet/FrontServlet.java
@@ -1,15 +1,18 @@
 package etu1283.framework.servlet;
 
 
+import etu1283.framework.Mapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class FrontServlet extends HttpServlet {
+    private HashMap<String, Mapping> mappingUrls;
 
     @Override
     protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException {
